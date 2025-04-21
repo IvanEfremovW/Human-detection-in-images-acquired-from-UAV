ed6ba79b6243420c7264c56f1ffc3a4ef924752b
IvanEfremovW <Ivan.EfremovWM@yandex.ru>
2025-04-21 14:07:32 +0300

    refactor: Добавлено указание принимаемых типов для внешних методов классов opengl_utils

--- src/image_processing/opengl_utils.py
+++ src/image_processing/opengl_utils.py
@@ -1,13 +1,16 @@
 from OpenGL.GL import *
 from OpenGL.GL.shaders import compileProgram, compileShader
+import OpenGL.constant
 
+import numpy as np
+from numpy.typing import NDArray
 
 class Shader:
-    def __init__(self, shader_type, filename):
+    def __init__(self, shader_type: OpenGL.constant.IntConstant, filename: str):
         self.id = self._load_shader(shader_type, filename)
 
     def _load_shader(self, shader_type, filename):
-        with open(filename, 'r') as file:
+        with open(filename, 'r', encoding='utf-8') as file:
             shader_code = file.read()
 
         shader = compileShader(shader_code, shader_type)
@@ -19,7 +22,7 @@ class Shader:
    
 
 class Program:
-    def __init__(self, *shaders):
+    def __init__(self, *shaders: OpenGL.constant.IntConstant):
         self.id = self._create_program(*shaders)
 
     def _create_program(self, *shaders):
@@ -28,11 +31,11 @@ class Program:
         
         return program
         
-    def set_uniform1i(self, name, value):
+    def set_uniform1i(self, name: str, value: int):
         location = glGetUniformLocation(self.id, name)
         glUniform1i(location, value)
     
-    def set_uniform1f(self, name, value):
+    def set_uniform1f(self, name: str, value: int):
         location = glGetUniformLocation(self.id, name)
         glUniform1f(location, float(value))
     
@@ -44,17 +47,17 @@ class Program:
 
 
 class Buffer:
-    def __init__(self, target):
+    def __init__(self, target: OpenGL.constant.IntConstant):
         self.id = glGenBuffers(1)
         self.target = target
 
     def bind(self):
         glBindBuffer(self.target, self.id)
     
-    def bind_base(self, index):
+    def bind_base(self, index: int):
         glBindBufferBase(self.target, index, self.id)
     
-    def data(self, data, usage):
+    def data(self, data: NDArray[np.float32], usage: OpenGL.constant.IntConstant):
         glBufferData(self.target, data.nbytes, data, usage)
 
     def unbind(self):
@@ -74,7 +77,15 @@ class VertexArray:
     def unbind(self):
         glBindVertexArray(0)
     
-    def linkAttrib(self, layout, size, type, normalized, stride, offset):
+    def linkAttrib(
+        self, 
+        layout: int, 
+        size: int, 
+        type: OpenGL.constant.IntConstant, 
+        normalized: OpenGL.constant.IntConstant, 
+        stride: int,
+        offset: int
+    ): 
         glVertexAttribPointer(layout, size, type, normalized, stride, GLvoidp(offset))
         glEnableVertexAttribArray(layout)
 
@@ -90,11 +101,11 @@ class Texture:
         self._set_parameters()
         self.unbind()
         
-    def bind(self, unit=0):
+    def bind(self, unit:int=0):
         glActiveTexture(GL_TEXTURE0 + unit)
         glBindTexture(GL_TEXTURE_2D, self.id)
 
-    def bind_image_texture(self, unit, access):
+    def bind_image_texture(self, unit: int, access: OpenGL.constant.IntConstant):
         glBindImageTexture(unit, self.id, 0, GL_FALSE, 0, access, GL_RGBA32F)
 
     def unbind(self):
@@ -106,8 +117,12 @@ class Texture:
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
 
-    def load_data(self, width, height, data, format=GL_RGBA):
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, width, height, 0, format, GL_UNSIGNED_BYTE, data)
+    def load_data(self, width: int,
+                  height: int,
+                  data: NDArray[np.uint8],
+                  format:OpenGL.constant.IntConstant=GL_RGBA
+    ):
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data)
     
     def delete(self):
         glDeleteTextures(1, [self.id])
