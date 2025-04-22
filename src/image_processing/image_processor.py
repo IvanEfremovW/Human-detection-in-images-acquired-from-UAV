49f3df1d16dfb1b883434faeedb843342cfcd457
IvanEfremovW <Ivan.EfremovWM@yandex.ru>
2025-04-22 20:31:48 +0300

    feat(python): Функционал обработки изображений вынесен в метод process

--- src/image_processing/image_processor.py
+++ src/image_processing/image_processor.py
@@ -6,17 +6,40 @@ import glfw
 from image_processing.opengl_utils import Shader, Program, Buffer, VertexArray, Texture
 
 class ImageProcessor:
-    def __init__(self, image_path, tile_size=16, clip_limit=5.0):
-        self.image_path = image_path
-        self.width, self.height, self.image_data = self._load_image()
-        
+    def __init__(self, tile_size=8, clip_limit=2.0):
         self.tile_size = tile_size
         self.clip_limit = clip_limit
         
-        self.NUM_BINS = 101
+        self.image_width = 0
+        self.image_height = 0
+        self.texture = None
+        
+        self.window = None
         
         self._create_context()
+        self._init_opengl_resources()
+
+        self.program.use()
         
+    def _create_context(self):
+        if not glfw.init():
+            raise Exception("GLFW initialization failed")
+        
+        glfw.window_hint(glfw.VISIBLE, False)
+        
+        self.window = glfw.create_window(2048, 2048, "Image Processing", None, None)
+        if not self.window:
+            glfw.terminate()
+            raise Exception("GLFW window creation failed")
+
+        glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 4)
+        glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+        glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+
+                
+        glfw.make_context_current(self.window)
+
+    def _init_opengl_resources(self):         
         self.vertices = np.array([
             -1.0, -1.0, 0.0,   0.0, 0.0,
              1.0, -1.0, 0.0,   1.0, 0.0,
@@ -37,7 +60,6 @@ class ImageProcessor:
         self.program.use()
         
         self.program.set_uniform1i('u_tileSize', self.tile_size)
-        self.program.set_uniform1i('u_numBins', self.NUM_BINS)
         self.program.set_uniform1f('u_clipLimit', self.clip_limit)
         
         self.vao = VertexArray()
@@ -56,78 +78,67 @@ class ImageProcessor:
         
         self.vao.unbind()
         self.vbo.unbind()
-        self.ebo.unbind()  
-        
-        self.texture = Texture()
-        
-        self.texture.bind()
-        self.texture.load_data(self.width, self.height, self.image_data)
-        self.texture.unbind()
-
-        self.program.use()
-        
-        
-    def _create_context(self):
-        if not glfw.init():
-            raise Exception("GLFW initialization failed")
-        
-        self.window = glfw.create_window(self.width, self.height, "Image Processing", None, None)
-        if not self.window:
-            glfw.terminate()
-            raise Exception("GLFW window creation failed")
-
-        glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 4)
-        glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
-        glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-        
-        glfw.window_hint(glfw.VISIBLE, False)
-                
-        glfw.make_context_current(self.window)
-        
-    def _load_image(self):
-        image = Image.open(self.image_path).convert("RGBA")
-        image = image.transpose(Image.FLIP_TOP_BOTTOM)
-        width, height = image.size
-        
-        image_data = np.array(image.getdata(), np.uint8).reshape(height, width, 4) # Ensure RGBA
-
-        return width, height, image_data
+        self.ebo.unbind() 
     
-    def draw(self):
+    def process(self, input_path, output_path):
+
+        if not self._load_image(input_path):
+            print("Failed to load image.")
+            return False
+        
+        texture = Texture()
+        
+        texture.bind()
+        texture.load_data(self.image_width, self.image_height, self.image_data)
+        texture.unbind()
+
+        self._draw(texture)
+
+        self._render_to_file(output_path)
+   
+    def _load_image(self, image_path):
+        
+        self.image_path = image_path
+        
+        try:
+            image = Image.open(self.image_path)
+            image = image.transpose(Image.FLIP_TOP_BOTTOM)
+            
+            self.image_width, self.image_height = image.size
+
+            self.image_data = np.array(image.convert("RGBA"), dtype=np.uint8)
+        except FileNotFoundError:
+            print(f"Error: File '{image_path}' not found.")
+            return False
+        except Exception as e:
+            print(f"Error loading image: {e}")
+            return False
+        return True
+    
+    def _draw(self, texture: Texture):
+        glClearColor(0.07, 0.13, 0.17, 1.0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+                
+        glViewport(0, 0, self.image_width, self.image_height)
+         
         self.program.use()
         
-        self.texture.bind()
+        texture.bind()
         self.vao.bind()
         
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, None)
         
         self.vao.unbind()
     
-    def render_to_file(self, output_path):
-        glClearColor(0.07, 0.13, 0.17, 1.0)
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-            
-        self.draw()
+    def _render_to_file(self, output_path):
 
-        pixels = glReadPixels(0, 0, self.width, self.height, GL_RGBA, GL_UNSIGNED_BYTE)
-        image = Image.frombuffer("RGBA", (self.width, self.height), pixels, "raw", "RGBA", 0, 0)
-        image = image.convert("RGB")  # Convert to RGB for JPEG compatibility
+        pixels = glReadPixels(0, 0, self.image_width, self.image_height, GL_RGBA, GL_UNSIGNED_BYTE)
+
+        image = Image.frombuffer("RGBA", (self.image_width, self.image_height), pixels, "raw", "RGBA", 0, 0)
         image.save(output_path)
-        
+
         self.delete()
 
-    def render_window(self):
-        while not glfw.window_should_close(self.window):
-            glClearColor(0.07, 0.13, 0.17, 1.0)
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-            
-            self.draw()
-            
-            glfw.swap_buffers(self.window)
-            glfw.poll_events()
-        
-        self.delete()
-        
     def delete(self):
         self.vao.delete()
         self.vbo.delete()
--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from image_processing.image_processor import ImageProcessor
 
 def main():
-    processor = ImageProcessor('images/image.jpg')
-    processor.render_window()
+    processor = ImageProcessor()
+    processor.process('input.png','output.png')
 
 if __name__ == "__main__":
     main()
