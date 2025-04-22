9aa95fbaf5acfdaad6a0282a9643e990fe86e7ee
IvanEfremovW <Ivan.EfremovWM@yandex.ru>
2025-04-22 20:40:04 +0300

    Delete src/main.py

--- src/main.py
+++
@@ -1,8 +0,0 @@
-from image_processing.image_processor import ImageProcessor
-
-def main():
-    processor = ImageProcessor('images/image.jpg')
-    processor.render_window()
-
-if __name__ == "__main__":
-    main()
